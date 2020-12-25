7613d2cff4e1e87ac5563c0891913387f1837e61
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2020-12-25 12:09:38 +0100

    Update setup.py (fix encoding problem and update requirements)

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 import aquasol
 
-# with open("README.md", "r") as f:  # Removed for now because of encoding issues
-    # long_description = f.read()
-
-long_description = 'see README.md'
+with open("README.md", "r", encoding='utf8') as f:
+    long_description = f.read()
 
 setup(
-        name='aquasol',
-        version=aquasol.__version__,
-        author='Olivier Vincent',
-        author_email='olivier.vincent@univ-lyon1.fr',
-        url='https://cameleon.univ-lyon1.fr/ovincent/thermo-ov',
-        description='Thermodynamic relations for water and solutions',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        packages=find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-        ],
-        setup_requires=[''],
-        python_requires='>=3.6'
+    name='aquasol',
+    version=aquasol.__version__,
+    author='Olivier Vincent',
+    author_email='olivier.vincent@univ-lyon1.fr',
+    url='https://cameleon.univ-lyon1.fr/ovincent/aquasol',
+    description='Thermodynamic relations for water and solutions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pynverse', 'numpy'],
+    setup_requires=['pytest'],
+    python_requires='>=3.6',
+    license='BSD-3-Clause'
 )
