8d0a300e917b5d8973fffbf9a50fe0fd1699f7fa
Olivier Vincent <ovinc.py@gmail.com>
2026-03-25 19:33:39 +0100

    Remove unnecessary if name main block in cacl2 refractive index module

--- aquasol/formulas/solutions/refractive_index/CaCl2.py
+++ aquasol/formulas/solutions/refractive_index/CaCl2.py
@@ -31,29 +31,3 @@ class RefractiveIndex_CaCl2_Tan(RefractiveIndex_CaCl2_Tan_Base):
 RefractiveIndexFormulas_CaCl2 = (
     RefractiveIndex_CaCl2_Tan,
 )
-
-# ====================== DIRECT RUN (test of formulas) =======================
-
-if __name__ == '__main__':
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    from aquasol.solutions import refractive_index
-
-    ws = 0.01, 0.05, 0.10, 0.15, 0.20, 0.25  # experimental weight fraction
-    ww = np.linspace(0, 0.25, 100)           # weight fraction for fit
-
-    nexps = {25: [1.3353, 1.3437, 1.3561, 1.3686, 1.3814, 1.3943], # 25°C
-             45: [1.3331, 1.3414, 1.3539, 1.3662, 1.3789, 1.3920]} # 45°C
-
-    fig, ax = plt.subplots()
-
-    for T, ns in nexps.items():
-
-        nn = refractive_index(w=ww, T=T, solute='CaCl2')
-
-        ax.plot(ws, ns, '.')
-        ax.plot(ww, nn, '-')
-
-    plt.show()
